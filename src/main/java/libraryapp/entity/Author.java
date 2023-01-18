5d5cac78b656740426a0ceb7cf22bee067f44791
Monia <monika.misiurek01@gmail.com>
2023-01-18 18:21:49 +0100

    Add author model

---
+++ src/main/java/libraryapp/entity/Author.java
@@ -0,0 +1,24 @@
+package libraryapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.UUID;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    private String name;
+    private LocalDate DateOfBirth;
+    private Set<Book> books = new HashSet<>();
+    private Set<Genre> genres = new HashSet<>();
+    private float rating;
+}
