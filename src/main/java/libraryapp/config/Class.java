ad3cb82b4c0e6edfe6b11382c0f41ae906fed5af
maciejkeller <maciej.keller@gmail.com>
2023-01-18 17:11:12 +0100

    review model

--- src/main/java/libraryapp/config/Class.java
+++
@@ -1,4 +0,0 @@
-package libraryapp.config;
-
-public class Class {
-}
--- src/main/java/libraryapp/entity/Review.java
+++ src/main/java/libraryapp/entity/Review.java
@@ -1,4 +1,32 @@
 package libraryapp.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    private UserApp username;
+    private LocalDate date;
+    private Set<Comments> comments = new HashSet<Comments>();
+    private Double rating;
+    private Book book;
 }
