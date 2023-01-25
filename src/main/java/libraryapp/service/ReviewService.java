ff95153fa4077fdaa69e6aa33412f57c7d626665
maciejkeller <maciej.keller@gmail.com>
2023-01-25 23:37:40 +0100

    Repository, Service and Controller for Reviews added

---
+++ src/main/java/libraryapp/service/ReviewService.java
@@ -0,0 +1,35 @@
+package libraryapp.service;
+
+import libraryapp.entity.Review;
+import libraryapp.repository.ReviewRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ReviewService {
+
+    private ReviewRepository reviewRepository;
+
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    public Optional<Review> findById(UUID id){
+        return reviewRepository.findById(id);
+    }
+
+    public Iterable<Review> findAllReviews(){
+        return reviewRepository.findAll();
+    }
+
+    public Review addReview(Review review){
+        return reviewRepository.save(review);
+    }
+
+    public void deleteById(UUID id){
+        reviewRepository.deleteById(id);
+    }
+
+}
