9b3032ad5c57cf8927626eef7e9597e6d085f738
maciejkeller <maciej.keller@gmail.com>
2023-02-02 23:28:19 +0100

    Review Mapper added, Repo, Service, Controller & DTO's updated

--- src/main/java/libraryapp/controller/ReviewController.java
+++ src/main/java/libraryapp/controller/ReviewController.java
@@ -1,44 +1,71 @@
 package libraryapp.controller;
 
+import libraryapp.dto.ReviewDtoIn;
+import libraryapp.dto.ReviewDtoOut;
 import libraryapp.entity.Review;
+import libraryapp.mapper.ReviewMapper;
 import libraryapp.service.ReviewService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/reviews")
 public class ReviewController {
 
-    private ReviewService reviewService;
+    private final ReviewService reviewService;
 
     public ReviewController(ReviewService reviewService) {
         this.reviewService = reviewService;
     }
 
-    @GetMapping("/all")
-    public Iterable<Review> getAll() {
-        return reviewService.findAllReviews();
-    }
-
-    @GetMapping
-    public Optional<Review> getById(@RequestParam UUID id) {
-        return reviewService.findById(id);
-    }
-
     @PostMapping
-    public Review addReview(@RequestBody Review review) {
-        return reviewService.addReview(review);
+    public ResponseEntity<Review> add(@RequestBody ReviewDtoIn reviewDtoIn) {
+        final Review review = reviewService.add(reviewDtoIn);
+        return ResponseEntity.status(HttpStatus.OK).body(review);
     }
 
-    @PutMapping
-    public Review updateReview(@RequestBody Review review) {
-        return reviewService.addReview(review);
+    @GetMapping("{id}")
+    public ResponseEntity<ReviewDtoOut> findById(@PathVariable UUID id) {
+        final Optional<Review> optionalReview = reviewService.findById(id);
+        return optionalReview.map(review -> ResponseEntity.ok(ReviewMapper.mapToDtoOut(review)))
+                .orElseGet(() -> ResponseEntity.of(Optional.empty()));
+    }
+
+    @GetMapping("{userId}")
+    public List<ReviewDtoOut> findByUserId(@PathVariable UUID userId){
+        return reviewService.findByUserId(userId).stream()
+                .map(ReviewMapper::mapToDtoOut)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("{bookId}")
+    public List<ReviewDtoOut> findByBookId(@PathVariable UUID bookId){
+        return reviewService.findByBookId(bookId).stream()
+                .map(ReviewMapper::mapToDtoOut)
+                .collect(Collectors.toList());
     }
 
     @DeleteMapping
-    public void deleteReview(@RequestParam UUID id) {
-        reviewService.deleteById(id);
+    public void deleteById (@RequestParam UUID reviewId){
+        reviewService.deleteById(reviewId);
     }
+
+    @PatchMapping("{reviewId}")
+    public void updateContent(@PathVariable UUID reviewId, @RequestParam String content){
+        reviewService.updateContent(reviewId,content);
+    }
+
+    @GetMapping
+    public List<ReviewDtoOut> findAll(){
+        return reviewService.findAll().stream()
+                .map(ReviewMapper::mapToDtoOut)
+                .collect(Collectors.toList());
+    }
+
 }
--- src/main/java/libraryapp/repository/ReviewRepository.java
+++ src/main/java/libraryapp/repository/ReviewRepository.java
@@ -6,13 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
 
-    public List<Review> findByBook_Id(UUID uuid);
+    public Optional<Review> findByBookId(UUID bookId);
+    public Optional<Review> findByUserId(UUID userId);
 
     @Query("select avg(r.rating) from Review as r where r.book.id='bookId'" )
     Double getBookRating (@Param("bookId") UUID bookId);
